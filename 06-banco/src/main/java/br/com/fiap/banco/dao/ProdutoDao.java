0458dc894192eda04ca1fccfac3022cae77ff039
Gustavo Carvalho <79180740+gustavocarvalho1997@users.noreply.github.com>
2023-09-19 21:27:36 -0300

    Add files via upload

--- 06-banco/src/main/java/br/com/fiap/banco/dao/ProdutoDao.java
+++ 06-banco/src/main/java/br/com/fiap/banco/dao/ProdutoDao.java
@@ -1,10 +1,69 @@
 package br.com.fiap.banco.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import br.com.fiap.banco.model.Produto;
 
 //Realiza as ações de CRUD (Create, Read, Update, Delete) no banco de dados
 public class ProdutoDao {
-	public void cadastrar(Produto produto) {
+	
+	public void cadastrar(Produto produto) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Obter a conexão com o banco de dados
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", "RM552466", "160297");
+		System.out.println("Conectado!");
 		
+		PreparedStatement stmt = conn.prepareStatement("INSERT INTO T_PRODUTO(CD_PRODUTO,NM_PRODUTO,"
+				+ "NR_ESTOQUE,VL_VENDA,VL_COMPRA) VALUES (?,?,?,?,?)");
+		
+		stmt.setInt(1, produto.getCodigo());
+		stmt.setString(2, produto.getNome());
+		stmt.setInt(3, produto.getNumeroEstoque());
+		stmt.setDouble(4, produto.getValorVenda());
+		stmt.setDouble(5, produto.getValorCompra());
+		
+		stmt.executeUpdate();
+		System.out.println("Produto cadastrado com sucesso");
+		
+		conn.close();
+	}
+	
+	public Produto pesquisarPorId(int id) throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Obter a conexão com o banco de dados
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", "RM552466", "160297");
+		System.out.println("Conectado!");
+		
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM T_PRODUTO WHERE CD_PRODUTO = ?");
+		stmt.setInt(1, id);
+		
+		ResultSet rs = stmt.executeQuery();
+		if(rs.next()) {
+			return new Produto(rs.getInt("CD_PRODUTO"), rs.getInt("NR_ESTOQUE"), rs.getString("NM_PRODUTO"), rs.getDouble("VL_VENDA"), rs.getDouble("VL_COMPRA"));
+		} else {
+			return null;
+			
+		}
+		
+	}
+	
+	public List<Produto> listar() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Obter a conexão com o banco de dados
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", "RM552466", "160297");
+		System.out.println("Conectado!");
+		
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM T_PRODUTO");
+		List<Produto> produtos = null;
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()) {
+			produtos.add(new Produto(rs.getInt("CD_PRODUTO"), rs.getInt("NR_ESTOQUE"), rs.getString("NM_PRODUTO"), rs.getDouble("VL_VENDA"), rs.getDouble("VL_COMPRA")));
+		}
+		return produtos;
 	}
 }//CLASS
--- 06-banco/src/main/java/br/com/fiap/banco/view/Terminal.java
+++ 06-banco/src/main/java/br/com/fiap/banco/view/Terminal.java
@@ -1,16 +1,27 @@
 package br.com.fiap.banco.view;
 
+import java.sql.SQLException;
+
 import br.com.fiap.banco.dao.ProdutoDao;
 import br.com.fiap.banco.model.Produto;
 
 public class Terminal {
 	public static void main(String[] args) {
 		//Instanciar um produto (a ideia é que o usuario informe os dados)
-		Produto produto = new Produto(1, 10, "Chocolate", 2, 1);
+		Produto produto = new Produto(4, 10, "Chocolate", 2, 1);
 		//Instanciar o objeto que é capaz de realizar as ações no banco de dados
 		ProdutoDao dao = new ProdutoDao();
 		//Gravar no banco
-		dao.cadastrar(produto);
+		try {
+			//dao.cadastrar(produto);
+			System.out.println(dao.listar());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}//MAIN
 }//CLASS
